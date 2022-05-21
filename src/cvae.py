67e59276be26cacdab4116ed1f669822de10c02c
nderus <nicolas.derus2@unibo.it>
2022-05-21 17:44:21 +0200

    tensorboard

---
+++ src/cvae.py
@@ -0,0 +1,115 @@
+import tensorflow as tf
+import keras
+
+class CVAE(keras.Model):
+    def __init__(self, encoder, decoder, beta, shape, **kwargs):
+        super(CVAE, self).__init__(**kwargs)
+        self.encoder = encoder
+        self.decoder = decoder
+        self.beta = beta
+        self.shape = shape
+        self.latent_var = []
+        self.total_loss_tracker = keras.metrics.Mean(name="total_loss")
+        self.reconstruction_loss_tracker = keras.metrics.Mean(
+            name="reconstruction_loss"
+        )
+        self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")
+        #
+        self.v_total_loss_tracker = keras.metrics.Mean(name="v_total_loss")
+        self.v_reconstruction_loss_tracker = keras.metrics.Mean(
+            name="v_reconstruction_loss")
+        self.v_kl_loss_tracker = keras.metrics.Mean(name="v_kl_loss")
+    
+    @property
+    def metrics(self):
+        return [
+            self.total_loss_tracker,
+            self.reconstruction_loss_tracker,
+            self.kl_loss_tracker,
+        ]
+       
+    def call(self, inputs):
+        _, input_label, conditional_input = self.conditional_input(inputs)
+        z_mean, z_log_var = self.encoder(conditional_input)
+        z_cond = self.sampling(z_mean, z_log_var, input_label)
+        return self.decoder(z_cond)
+    
+    def conditional_input(self, inputs, label_size=10): 
+  
+        image_size = [self.shape[0], self.shape[1], self.shape[2]]
+    
+        input_img = layers.InputLayer(input_shape=image_size,
+                                      dtype ='float32')(inputs[0])
+        input_label = layers.InputLayer(input_shape=(label_size, ),
+                                        dtype ='float32')(inputs[1])
+
+        labels = tf.reshape(inputs[1], [-1, 1, 1, label_size])
+        labels = tf.cast(labels, dtype='float32')
+        ones = tf.ones([inputs[0].shape[0]] + image_size[0:-1] + [label_size]) 
+        labels = ones * labels
+        conditional_input = layers.Concatenate(axis=3)([input_img, labels]) 
+        return  input_img, input_label, conditional_input
+
+    def sampling(self, z_mean, z_log_var, input_label):
+        if len(input_label.shape) == 1:
+            input_label = np.expand_dims(input_label, axis=0)
+
+        eps = tf.random.normal(tf.shape(z_log_var), dtype=tf.float32,
+                               mean=0., stddev=1.0, name='epsilon')
+        z = z_mean + tf.exp(z_log_var / 2) * eps
+        z_cond = tf.concat([z, input_label], axis=1)
+        return z_cond
+
+    def train_step(self, data):
+
+        if isinstance(data, tuple):
+            data = data[0]
+        with tf.GradientTape() as tape:
+        
+            input_img, input_label, conditional_input = self.conditional_input(data)
+            z_mean, z_log_var = self.encoder(conditional_input)
+            self.latent_var.append(z_log_var)
+            z_cond = self.sampling(z_mean, z_log_var, input_label)
+            reconstruction = self.decoder(z_cond)
+            reconstruction_loss = np.prod(self.shape) * tf.keras.losses.MSE(tf.keras.backend.flatten(input_img),
+                                    tf.keras.backend.flatten(reconstruction))            
+            kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean)
+                      - tf.exp(z_log_var))
+            kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1)) # was just reduce_sum
+            kl_loss = self.beta * kl_loss
+            total_loss = reconstruction_loss + kl_loss
+            #total_loss = tf.reduce_mean(total_loss) #not necessary since I added red mean in kl_loss
+        grads = tape.gradient(total_loss, self.trainable_weights)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+        return {
+            "loss": self.total_loss_tracker.result(),
+            "reconstruction_loss": self.reconstruction_loss_tracker.result(),
+            "kl_loss": self.kl_loss_tracker.result(),
+        }
+
+    def test_step(self, data):
+        if isinstance(data, tuple):
+            data = data[0]
+        input_img, input_label, conditional_input = self.conditional_input(data)
+        z_mean, z_log_var = self.encoder(conditional_input)
+        z_cond = self.sampling(z_mean, z_log_var, input_label)
+        reconstruction = self.decoder(z_cond)
+        reconstruction_loss = np.prod(self.shape) * tf.keras.losses.MSE(tf.keras.backend.flatten(input_img),
+                             tf.keras.backend.flatten(reconstruction)) # over weighted MSE    
+
+        kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean)
+                  - tf.exp(z_log_var))
+        kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
+        kl_loss = self.beta * kl_loss
+        total_loss = reconstruction_loss + kl_loss
+        self.total_loss_tracker.update_state(total_loss)
+        self.reconstruction_loss_tracker.update_state(reconstruction_loss)
+        self.kl_loss_tracker.update_state(kl_loss)
+        return{
+            'loss': total_loss,
+            'reconstruction_loss': reconstruction_loss,
+            'kl_loss': kl_loss
+        }
